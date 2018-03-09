4faee8fa450ceaf061904c796c1304c5f87eeff0
Piotr Szumny <piotr.advox@gmail.com>
2018-03-09 19:18:03 +0100

    logs-proposal

--- cmd/cli/cmd/rush.go
+++ cmd/cli/cmd/rush.go
@@ -1,14 +1,15 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
-	"log"
 	"os"
 	"rusher/internal/platform/container"
+	"rusher/internal/platform/log"
 	"rusher/internal/platform/rollback"
-	"rusher/internal/step"
 	"rusher/internal/platform/schema"
+	"rusher/internal/step"
 )
 
 var schemaFlag string
@@ -20,7 +21,7 @@ func init() {
 	rootCmd.AddCommand(rushCmd)
 }
 
-var rushCmd = &cobra.Command{
+var rushCmd *cobra.Command = &cobra.Command{
 	Use:   "rush",
 	Short: "Executes rusher's steps",
 	Long:  "Parses schema, validates and executes steps. On error rollbacks.",
@@ -32,11 +33,11 @@ var rushCmd = &cobra.Command{
 		fmt.Println("Trying to rush " + envFlag + " environment from " + schemaFlag)
 		s, err := schema.New(schemaFlag)
 		if err != nil {
-			log.Fatal("Could not fetch schema. Please ensure that given schema path is correct [" + err.Error() + "]")
+			log.Fatal(log.StdOutLog, errors.New("Could not fetch schema. Please ensure that given schema path is correct ["+err.Error()+"]"))
 		}
 		currentConfig, err := schema.GetCurrentConfig(s, envFlag)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(log.StdOutLog, err)
 		}
 
 		fmt.Println("Rushing...")
@@ -44,12 +45,11 @@ var rushCmd = &cobra.Command{
 
 		switch err.(type) {
 		case *step.ErrInvalidStep:
-			log.Fatal(err)
+			log.Fatal(log.StdOutLog, err)
 			break
 		case error:
-			err := doRollback(err, currentConfig)
-			if err != nil {
-				log.Fatal(err)
+			if err := doRollback(err, currentConfig); err != nil {
+				log.Fatal(log.StdOutLog, err)
 			}
 			break
 		}
